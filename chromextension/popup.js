1b55c414feb6d126760c40c821e70ce93c8a0cca
Teraoka-Yuta <c1103854@planet.kanazawa-it.ac.jp>
2023-12-07 09:04:32 +0900

    ミスの修正とJavaScriptのvarを削除

--- chromextension/popup.js
+++ chromextension/popup.js
@@ -1,13 +1,15 @@
+let content
 document.addEventListener('DOMContentLoaded', function() {
     // ポップアップが表示されるたびに保存された内容を読み込む
     chrome.storage.sync.get(['content'], function(result) {
-      var content = result.content || '';
+      content = result.content || '';
       document.getElementById('contentArea').value = content;
+      document.getElementById('task_1').textContent = content;
     });
   
     // テキストエリアの内容が変更されたら保存する
     document.getElementById('contentArea').addEventListener('input', function() {
-      var content = this.value;
+      content = this.value;
       chrome.storage.sync.set({ 'content': content });
     });
 
